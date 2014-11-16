34e503827a06a9fc870e867b5553db6c14638a63
ray-z <710956@gmail.com>
2014-11-16 18:18:38 +0000

    switch to ubuntu

--- cw1.c
+++ cw1.c
@@ -7,49 +7,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define MAX_RAND 100   /* maximum random number */
 
+int isEnd = 0;
+pthread_barrier_t barrier;
+
 void print2DArr(int *arr, int len);
 void copyArr(int *dst, int *src, int len);
-int averaging(int *arr, int *temp, int row, int col, int precision);
+void averaging(int *arr, int row, int col, int precision);
 
 int main(int argc, char *argv[])
 {
+    /* get command-line arguments 
+     *
+     * default value:
+     * square array length: 10
+     * precision: max value of the random range, so the loop will run once only
+     * number of threads: 1
+     */
     int arrLen, precision, numThreads;
-    int *randArr, *tempArr;
+    arrLen = (argv[1]) ? strtol(argv[1], NULL, 10) : 10;
+    precision = (argv[2]) ? strtol(argv[2], NULL, 10) : MAX_RAND;
+    numThreads = (argv[3]) ? strtol(argv[3], NULL, 10) : 1;
 
-    /* get command-line arguments */
-    arrLen = (argv[1]) ? strtol(argv[1], NULL, 10) : 0;
-    precision = (argv[2]) ? strtol(argv[2], NULL, 10) : 0;
-    numThreads = (argv[3]) ? strtol(argv[3], NULL, 10) : 0;
 
-    /* init a array */
+    /* init array */
+    int randArr[arrLen*arrLen];
+    //int tempArr[arrLen*arrLen];
     printf("Random generating a 2D array with dimension = %d:\n", arrLen);
-    randArr = (int *)malloc(arrLen*arrLen*sizeof(int)); 
-    tempArr = (int *)malloc(arrLen*arrLen*sizeof(int)); 
     for(int i = 0; i < arrLen*arrLen; i++)
     {
         randArr[i] = rand() % MAX_RAND;
     }
-    copyArr(tempArr, randArr, arrLen*arrLen);
-
     //copyArr(tempArr, randArr, arrLen*arrLen);
     print2DArr(randArr, arrLen);
+
+
+    /* create threads */
+    pthread_t threads[numThreads];
+
+    //for(;;)
+    //{
+        
+   // }
+
+
+
+
+
     
     /* do averaging */
     printf("Averaging...\n");
     printf("Result:\n");
-    while(averaging(randArr, tempArr, arrLen, arrLen, precision))
+    while(!isEnd)
     {
-        copyArr(tempArr, randArr, arrLen*arrLen);
+        averaging(randArr, arrLen, arrLen, precision);
+        
     }
     print2DArr(randArr, arrLen);
 
 
 
-    free(randArr);
-    free(tempArr);
     return 0;
 }
 
@@ -57,21 +77,22 @@ int main(int argc, char *argv[])
  * averaging: replacing a value with the average of its four neighbours
  * return: number of items whose value >= max after averaging
  */
-int averaging(int *arr, int *temp, int row, int col, int max)
+void averaging(int *arr, int row, int col, int max)
 {
-    int counter = 0;
+    int temp[row*col];
+    copyArr(temp, arr, row*col);
+
+    isEnd = 1;
+
     for(int r = 1; r < row-1; r++)
     {
         for(int c = 1; c < col-1; c++)
         {
             int avg = (temp[r*col + c - 1] + temp[r*col + c + 1] + 
                     temp[(r-1)*col + c] + temp[(r+1)*col +c]) / 4; 
-            if((arr[r*col + c] = avg) >= max)  counter++;
-            /*printf("result=%d, precision=%d, counter=%d\n", 
-                    result, precision, counter);*/
+            if((arr[r*col + c] = avg) >= max)  isEnd = 0;
         }
     }
-    return counter;
 }
 
 /*
